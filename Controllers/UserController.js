2196892cc9d8264b70861dcfeae9c5ee843bdb40
mannshrimali2110 <mannshrimali2003@gmail.com>
2025-01-10 14:35:49 +0530

    3rd Commit - Admin Login and Signup added with JWT authentication

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -35,6 +35,9 @@ const AddUser = async (req, res) => {
 const LoginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
+        if (!email || !password) {
+            return res.json({ message: 'All Fields are required' })
+        }
         const User = await UserData.findOne({ email });
         console.log(User);
         if (!User) {
@@ -57,4 +60,64 @@ const LoginUser = async (req, res) => {
     }
 }
 
-module.exports = { AddUser, LoginUser };
+const AddAdmin = async (req, res) => {
+    try {
+        const { username, email, password, adminkey } = req.body;
+        if (!username || !email || !password || !adminkey) {
+            return res.json({ message: 'All Fields are required' });
+        }
+        const UserExists = await UserData.findOne({ email });
+        if (UserExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        if (adminkey != process.env.ADMIN_KEY) {
+            return res.status(400).json({ message: "Invalid Credentials|Admin Key" });
+        }
+
+        const NewUser = await UserData.create({ username, email, password, role: 'Admin' });
+        const token = generateToken(NewUser._id);
+        await NewUser.save();
+        return res.status(201).json(({
+            user: {
+                id: NewUser._id, email: NewUser.email
+            }, token
+        }));
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const LoginAdmin = async (req, res) => {
+    try {
+        const { email, password, adminkey } = req.body;
+        if (!email || !password || !adminkey) {
+            return res.json({ message: 'All Fields are required' })
+        }
+        const User = await UserData.findOne({ email });
+        // console.log(User);
+        if (!User) {
+            return res.status(404).json({ message: "User Does Not Exist" });
+        }
+        if (adminkey != process.env.ADMIN_KEY) {
+            return res.status(400).json({ message: "Invalid Credentials|Admin Key" });
+        }
+        const isPasswordValid = await bycrpt.compare(password, User.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+        const token = generateToken(User._id);
+        return res.status(201).json(({
+            user: {
+                id: User._id, email: User.email
+            }, token
+        }));
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+
+    }
+}
+
+
+module.exports = { AddUser, LoginUser,AddAdmin,LoginAdmin };
--- Routes/apiRoute1.js
+++ Routes/apiRoute1.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.post('/auth/signup', UserController.AddUser);
 router.post('/auth/login', UserController.LoginUser);
+router.post('/auth/adminsignup', UserController.AddAdmin);
+router.post('/auth/adminlogin', UserController.LoginAdmin);
 router.get('/auth/protected',authMiddleware.protect);
 
 module.exports = router;
